cee9d0df6214fe036972b32f22b4ec359f84f1f3
Frank Chen <chentao76@hotmail.com>
2024-05-28 21:33:40 -0700

    added comments

--- EntraID/GetSignInsInfo.ps1
+++ EntraID/GetSignInsInfo.ps1
@@ -1,3 +1,23 @@
+## This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.  
+## THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, 
+## INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  
+## We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object code form of the Sample Code, 
+## provided that You agree: (i) to not use Our name, logo, or trademarks to market Your software product in which the Sample Code is embedded; 
+## (ii) to include a valid copyright notice on Your software product in which the Sample Code is embedded; and (iii) to indemnify, hold harmless, 
+## and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code.
+## Please note: None of the conditions outlined in the disclaimer above will supersede the terms and conditions contained within the Premier Customer Services Description.
+##
+## Description 
+## the PS script inventory enterprise application from EntraID
+##
+## Instruction 
+## 1. Update $clientId to point to AAD application Id 
+## 2. Update $tenantName to point to AAD tenant name
+## 3. Update $thumbprint to include correct certificate thumbprint
+## 4. Update $siteUrl to point to a SPO site collection
+## Output: 
+## 1. A CSV file with the list of enterprise applications and their sign-ins information
+
 $scriptFullPath = $MyInvocation.MyCommand.Path
 $scriptPath = Split-Path $scriptFullPath
 
